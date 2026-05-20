74d18baf74379ebb4d322006a4586fe8ff672ba6
Luis Silva <luis.m.silva8@edu.senai.br>
2026-05-20 10:50:18 -0300

    feat: adiciona classificação e ralatorio

--- main.cpp
+++ main.cpp
@@ -31,33 +31,59 @@ int main()
 
     do
     {
-        cout << "\nQuantidade de disciplinas (1 a 5):" << endl;
+        cout << "\nQuantidade de disciplinas (1 a 5): ";
         cin >> qtdDisciplinas;
     } while (qtdDisciplinas < 1 || qtdDisciplinas > 5);
 
     for (int i = 0; i < qtdAlunos; i++)
     {
-        cout << "\nNotas de " << nomes[i] << ": " << endl;
+        cout << "\nNotas de " << nomes[i] << ":" << endl;
         float soma = 0;
         for (int j = 0; j < qtdDisciplinas; j++)
         {
             do
             {
-                cout << "Disciplina" << j + 1 << "(1 a 5):";
+                cout << "  Disciplina " << j + 1 << " (0 a 10): ";
                 cin >> notas[i][j];
-            } while (notas[i][j] < 1 || notas[i][j] > 5);
+            } while (notas[i][j] < 0 || notas[i][j] > 10);
+            
             soma += notas[i][j];
         }
         media[i] = soma / qtdDisciplinas;
     }
 
     // Saída
-    cout << "\nAlunos cadastrados:" << endl;
-
+    cout << "\n=== ALUNOS CADASTRADOS ===" << endl;
     for (int i = 0; i < qtdAlunos; i++)
     {
         cout << " " << i + 1 << ". " << nomes[i] << endl;
     }
+
+    cout << "\n=== RELATORIO DE NOTAS ===" << endl;
+    int aprovados = 0, recuperacao = 0, reprovados = 0;
+
+    for (int i = 0; i < qtdAlunos; i++)
+    {
+        cout << " " << nomes[i] << " Media: " << media[i] << " - Status: ";
+        if (media[i] >= 7.0)
+        {
+            cout << "Aprovado" << endl;
+            aprovados++;
+        }
+        else if (media[i] >= 5.0)
+        {
+            cout << "Recuperacao" << endl;
+            recuperacao++;
+        }
+        else
+        {
+            cout << "Reprovado" << endl;
+            reprovados++;
+        }
+    }
+
+    cout << "\n=== RESUMO FINAL ===" << endl;
+    cout << " Aprovados: " << aprovados << " | Em Recuperacao: " << recuperacao << " | Reprovados: " << reprovados << endl;
 
     return 0;
 }
